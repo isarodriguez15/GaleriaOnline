24763ad73590d942bcf49776f0037e0a4094ea84
Isabelle Rodrigues <isabelle.r.reis@aluno.senai.br>
2025-08-21 09:24:22 -0300

    :tada: subindo novo projeto da api

---
+++ GaleriaOnline.WebApi/Program.cs
@@ -0,0 +1,54 @@
+
+using GaleriaOnline.WebApi.DbContextImagem;
+using GaleriaOnline.WebApi.Interface;
+using GaleriaOnline.WebApi.Repository;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IImagemRepository, ImagemRepository>();
+builder.Services.AddDbContext<GaleriaOnlineDbContext>
+    (options => options.UseSqlServer
+    (builder.Configuration.GetConnectionString
+    ("DefaltConnection")));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+
+    });
+});
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
